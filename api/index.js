17254d63818bb06967318b39d08e1449a12c5320
Erdem Ozdemir <erdemozproductions@gmail.com>
2024-03-01 12:26:26 -0500

    implemented cors

--- api/index.js
+++ api/index.js
@@ -9,7 +9,12 @@ const schema = require('./schema/schema')
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: 'https://devjobs.erdemoz.io/', 
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
 
 connectDB()
 
