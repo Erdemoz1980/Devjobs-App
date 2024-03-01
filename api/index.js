3304debdee8b327e530de19973f5304faa78e2cf
Erdem Ozdemir <erdemozproductions@gmail.com>
2024-03-01 13:29:21 -0500

    backend cors issue fix 2.0

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 const corsOptions = {
-  origin: '*', 
+  origin: 'https://devjobs.erdemoz.io/', 
   credentials: true, 
 };
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,7 @@ const cache = new InMemoryCache({
 })
 
 const client = new ApolloClient({
-  uri: "https://devjobs-hoi4orvp0-erdemoz1980s-projects.vercel.app/",
+  uri: "https://vercel.com/_next/static/chunks/59157-155d91716d59ef39.js?dpl=dpl_AC8Kfwk9KzUVuQfNYJ4B24p9woom",
   cache
 })
 
